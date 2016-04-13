db59f6b6bbf75958ae4245ca7835983c2a72e21c
Prelang Builder <builder@prelang.com>
2016-04-13 02:43:40 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'JobType' to allow for create, update

--- app/admin/job_type.rb
+++ app/admin/job_type.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register JobType do
+  permit_params :type_of_jobs
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
