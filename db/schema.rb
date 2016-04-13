5d69824f0998f1147e61c32bdcdbf72b9f66d9fe
Prelang Builder <builder@prelang.com>
2016-04-13 10:50:46 +0800

    Executed 'rake db:migrate' (development and test) to migrate all generated migrations which created schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413024220) do
+ActiveRecord::Schema.define(version: 20160413024322) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -46,6 +46,18 @@ ActiveRecord::Schema.define(version: 20160413024220) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
 
+  create_table "certifications", force: true do |t|
+    t.integer  "profile_id"
+    t.string   "certification_name"
+    t.string   "certification_authority"
+    t.string   "certification_url"
+    t.string   "certification_image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "certifications", ["profile_id"], name: "index_certifications_on_profile_id"
+
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
     t.integer  "attempts",   default: 0, null: false
@@ -61,5 +73,137 @@ ActiveRecord::Schema.define(version: 20160413024220) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+
+  create_table "educations", force: true do |t|
+    t.string   "school_name"
+    t.string   "course"
+    t.string   "field_of_study"
+    t.text     "year_graduated"
+    t.integer  "profile_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "educations", ["profile_id"], name: "index_educations_on_profile_id"
+
+  create_table "experiences", force: true do |t|
+    t.string   "work_position"
+    t.string   "company_name"
+    t.datetime "date_started"
+    t.datetime "date_ended"
+    t.float    "time_in_service"
+    t.integer  "profile_id"
+    t.text     "company_description"
+    t.string   "company_image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "experiences", ["profile_id"], name: "index_experiences_on_profile_id"
+
+  create_table "job_categories", force: true do |t|
+    t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "job_posts", force: true do |t|
+    t.string   "company_name"
+    t.string   "job_title"
+    t.string   "salary"
+    t.string   "location"
+    t.text     "job_description"
+    t.text     "job_benefits"
+    t.string   "contact_email"
+    t.string   "website"
+    t.string   "phone"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "job_posts", ["user_id"], name: "index_job_posts_on_user_id"
+
+  create_table "job_types", force: true do |t|
+    t.string   "type_of_jobs"
+    t.integer  "job_post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "job_types", ["job_post_id"], name: "index_job_types_on_job_post_id"
+
+  create_table "profiles", force: true do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "position"
+    t.text     "summary"
+    t.string   "mobile_phone"
+    t.string   "telephone"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
+
+  create_table "projects", force: true do |t|
+    t.integer  "profile_id"
+    t.string   "project_name"
+    t.string   "project_image"
+    t.text     "project_description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "projects", ["profile_id"], name: "index_projects_on_profile_id"
+
+  create_table "skills", force: true do |t|
+    t.string   "skill_name"
+    t.integer  "skill_level"
+    t.integer  "profile_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "skills", ["profile_id"], name: "index_skills_on_profile_id"
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.string   "username"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
+
+  create_table "votes", force: true do |t|
+    t.integer  "votable_id"
+    t.string   "votable_type"
+    t.integer  "voter_id"
+    t.string   "voter_type"
+    t.boolean  "vote_flag"
+    t.string   "vote_scope"
+    t.integer  "vote_weight"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
