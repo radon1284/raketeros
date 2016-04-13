8a4fc56a011e47addf99f3065ac54c580773fa80
Prelang Builder <builder@prelang.com>
2016-04-13 02:44:06 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,5 +1,5 @@
 class SkillsController < ApplicationController
-  before_action :set_skill, only: [:show, :edit, :update, :destroy]
+  before_action :set_skill, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /skills
   # GET /skills.json
