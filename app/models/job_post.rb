0708ccfbabe173fe01ab05b54bee0cf33b377df5
Prelang Builder <builder@prelang.com>
2016-04-13 02:43:23 +0000

    Defined validation for attribute 'website' on model 'JobPost'

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -2,4 +2,5 @@ class JobPost < ActiveRecord::Base
   belongs_to :user
   has_many :job_types
   validates_formatting_of :contact_email, using: :email
+  validates_formatting_of :website, using: :url
 end
